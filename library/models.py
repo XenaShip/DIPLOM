1636dddcf3372364158ce5b3a73420f535982d38
shipx <shipxena@gmail.com>
2024-09-18 14:42:56 +0300

    2

--- library/models.py
+++ library/models.py
@@ -13,7 +13,7 @@ class Books(models.Model):
     name = models.CharField(max_length=100, verbose_name='название книги')
     availability = models.BooleanField(verbose_name='наличие книги')
     genre = models.CharField(max_length=100, verbose_name='жанр книги')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='пользователь')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='пользователь', **NULLABLE)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, verbose_name='автор книги')
 
     def __str__(self):
--- library/views.py
+++ library/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
@@ -16,8 +16,18 @@ class BooksCreateApiView(CreateAPIView):
 class BooksListApiView(ListAPIView):
     queryset = Books.objects.all()
     serializer_class = BooksSerializer
-    pagination_class = BooksPaginator
-    filter_backends = [SearchFilter,]
+    # pagination_class = BooksPaginator
+    # filter_backends = [SearchFilter, OrderingFilter]
+    # search_fields = ('name', 'availability', 'genre', 'user', 'author')
+    # ordering_fields = ('name',)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        print(queryset.all())
+        if self.request.query_params.get('genre'):
+            queryset = queryset.filter(genre=str(self.request.query_params.get('genre')))
+            print('cool', queryset.all())
+        return queryset
 
 
 class BooksRetrieveApiView(RetrieveAPIView):
@@ -60,3 +70,9 @@ class AuthorUpdateApiView(UpdateAPIView):
 class AuthorDestroyApiView(DestroyAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+
+
+class BooksSearchApiView(ListAPIView):
+    queryset = Books.objects.all()
+    serializer_class = BooksSerializer
+
