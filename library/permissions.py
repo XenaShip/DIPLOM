5df407461c4dc9986ab9ed4acb974264ce6672ed
shipx <shipxena@gmail.com>
2024-09-19 13:31:40 +0300

    1

--- library/permissions.py
+++ library/permissions.py
@@ -4,4 +4,4 @@ from rest_framework.permissions import BasePermission
 class IsLibrarian(BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        return request.user.is_staff
+        return request.user.groups.filter(name='Librarian').exists()
