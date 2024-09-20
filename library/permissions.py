991d2dc3b2e9db59282fe736d4eec14d6bae78e5
shipx <shipxena@gmail.com>
2024-09-20 19:39:28 +0300

    1

--- library/permissions.py
+++ library/permissions.py
@@ -2,6 +2,6 @@ from rest_framework.permissions import BasePermission
 
 
 class IsLibrarian(BasePermission):
-
+    """Проверка, является ли пользователь Библиотекарем"""
     def has_object_permission(self, request, view, obj):
         return request.user.groups.filter(name='Librarian').exists()
--- library/views.py
+++ library/views.py
@@ -8,6 +8,10 @@ from library.models import Books, Author
 from library.pagination import BooksPaginator, AuthorPaginator
 from library.permissions import IsLibrarian
 from library.serializer import BooksSerializer, AuthorSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+
+
+"""Контроллеры для книг"""
 
 
 class BooksCreateApiView(CreateAPIView):
@@ -20,8 +24,8 @@ class BooksListApiView(ListAPIView):
     queryset = Books.objects.all()
     serializer_class = BooksSerializer
     pagination_class = BooksPaginator
-    filter_backends = [SearchFilter,]
-    search_fields = ['genre', 'name', 'user',]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['genre', 'name', 'user', 'availability', 'author']
 
 
 class BooksRetrieveApiView(RetrieveAPIView):
@@ -41,18 +45,21 @@ class BooksDestroyApiView(DestroyAPIView):
     permission_classes = [~IsLibrarian]
 
 
+"""Контроллеры для Авторов"""
+
+
 class AuthorCreateApiView(CreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
-    permission_classes = [~IsLibrarian]
+    permission_classes = [~IsLibrarian,]
 
 
 class AuthorListApiView(ListAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
     pagination_class = AuthorPaginator
-    filter_backends = [SearchFilter,]
-    search_fields = ['name',]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name',]
 
 
 class AuthorRetrieveApiView(RetrieveAPIView):
